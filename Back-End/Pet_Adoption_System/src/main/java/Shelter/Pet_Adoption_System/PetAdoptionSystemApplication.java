abd4ead00f34b0a6b630db8c260fabd0f0f8f0f2
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2023-12-22 04:13:56 +0200

    Initial Commit
    Initialized React-ts project and springboot project

---
+++ Back-End/Pet_Adoption_System/src/main/java/Shelter/Pet_Adoption_System/PetAdoptionSystemApplication.java
@@ -0,0 +1,13 @@
+package Shelter.Pet_Adoption_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionSystemApplication.class, args);
+	}
+
+}
---
+++ Back-End/Pet_Adoption_System/src/test/java/Shelter/Pet_Adoption_System/PetAdoptionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Shelter.Pet_Adoption_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
