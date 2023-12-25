bcb3db2fcc9a4f3cf253a93e7dde65cd415c0c0c
mostafa336 <96492072+mostafa336@users.noreply.github.com>
2023-12-25 12:53:00 +0200

    Adaptor front end

--- Back-End/Pet_Adoption_System/src/main/java/Shelter/Pet_Adoption_System/model/Person/Person.java
+++ Back-End/Pet_Adoption_System/src/main/java/Shelter/Pet_Adoption_System/model/Person/Person.java
@@ -2,20 +2,16 @@ package Shelter.Pet_Adoption_System.model.Person;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
 
 @Data
 @MappedSuperclass
 @NoArgsConstructor
 @AllArgsConstructor
-public abstract class Person implements UserDetails {
+public abstract class Person {
     @Column(name = "name")
     protected String name;
 
-    @Column(name = "email_address", unique = true, nullable = false)
+    @Column(name = "email_address")
     protected String emailAddress;
 
     @Column(name = "phone_number")
@@ -23,39 +19,4 @@ public abstract class Person implements UserDetails {
 
     @Column(name = "password_hash")
     protected String passwordHash;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return emailAddress;
-    }
-
-    @Override
-    public String getPassword() {
-        return passwordHash;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
