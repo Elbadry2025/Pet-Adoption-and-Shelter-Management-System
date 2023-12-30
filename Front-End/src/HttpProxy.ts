e1130bb7bf11dcabe565c3cda980f19450d6a6ef
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2023-12-30 05:24:17 +0200

    Fix conflicts

--- Front-End/src/HttpProxy.ts
+++ Front-End/src/HttpProxy.ts
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-
-import axios from "axios";
-//import { getJwtToken } from "../CurrentSession";
-
-
-axios.defaults.baseURL = "http://localhost:8081";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-
-
-export const httpRequest = (method: string, url: string, data?: any) => {
-  let headers = {};
-
-//   if (getJwtToken() !== null && getJwtToken() !== "null")
-//     headers = { Authorization: `Bearer ${getJwtToken()}` };
-
-  return axios({
-    method: method,
-    headers: headers,
-    url: url,
-    data: data,
-  });
-};
-=======
 import axios from "axios";
 import { getToken } from "./CurrentSession";
 
@@ -44,4 +20,4 @@ export const httpRequest = (method: string, url: string, data?: any) => {
     data: data,
   });
 };
->>>>>>> Stashed changes
+
